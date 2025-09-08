7eaa158c149b1c409852b00e91e8e26133311d4d
yonahara <yonahara.y.24kdgn@gmail.com>
2025-09-08 13:41:24 +0900

    completion

--- laravel_app/resources/views/Page/UserSide/oder_end.blade.php
+++ laravel_app/resources/views/Page/UserSide/oder_end.blade.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>注文完了ページ</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css">
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 <body>
-    
+    <h1 class="completion">注文完了しました</h1>
+    <img src="../../img/image17.svg" alt="" class="img_1">
 </body>
 </html>
