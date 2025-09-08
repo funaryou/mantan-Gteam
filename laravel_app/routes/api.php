fc11ba1c4f89d39289c18c175fc0fb08508c1076
funa-div <funahashi.ryouma@gmail.com>
2025-09-08 13:40:10 +0900

    create_rotes

---
+++ laravel_app/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Events\RouteMatched;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MenuSearchController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('/client')->group(function () {
+    Route::prefix('/menu')->group(function () {
+        Route::post('/search/{id}/{id}', [MenuSearchController::class, 'search'])->name('client.menu.search');
+    });
+});
